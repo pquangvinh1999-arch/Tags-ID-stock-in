99a6487a093b3b20dcdf0eff5a6f7ce612d6ee13
Vinh <pquangvinh1999@gmail.com>
2026-05-09 12:06:22 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,20 @@
 const CACHE_NAME = 'whm-scan-v1';
-const urlsToCache = ['index.html', 'manifest.json'];
+// Thêm ./ để trình duyệt tìm file ngay tại thư mục hiện tại của App
+const urlsToCache = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Caching app shell...');
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
